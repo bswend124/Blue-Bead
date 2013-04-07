f4ace599e98e7939b2d9e7e413c41268b276e485
halben <roluok87@gmail.com>
2013-04-07 18:55:37 -0500

    Update functions.php
    Changed register_my_menu to register_my_menus to give user multiple custom
    navigation menu. Added a top-menu/bottom-menu.

--- wp-content/themes/bluebead/functions.php
+++ wp-content/themes/bluebead/functions.php
@@ -1,7 +1,7 @@
 <?php
  
 //Add support for WordPress 3.0's custom menus
-add_action( 'init', 'register_my_menu' ); 
+add_action( 'init', 'register_my_menus' ); 
 
 // Adding theme support for custom backgrounds
 add_custom_background();
@@ -12,8 +12,13 @@ add_theme_support( 'automatic-feed-links' );
 // Adding theme support for post thumbnails
 add_theme_support( 'post-thumbnails' );
 
-//Register area for custom menu
-function register_my_menu() {
-    register_nav_menu( 'primary-menu', __( 'Primary Menu' ) );
+// Register area for custom menus
+// Give users options to add a top/bottom menu
+function register_my_menus() {
+    register_nav_menus( array(
+ 	      'top-menu' => __( 'Top Menu' ),
+		      'bottom-menu' => __( 'Bottom Menu' )
+        )
+   );
 }
 ?>
