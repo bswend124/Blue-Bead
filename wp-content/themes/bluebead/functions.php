50e05f571e448d1e30bf6072753d4e125eb1dcdb
halben <roluok87@gmail.com>
2013-04-06 20:38:54 -0500

    Update functions.php

--- wp-content/themes/bluebead/functions.php
+++ wp-content/themes/bluebead/functions.php
@@ -1,12 +1,19 @@
 <?php
  
 //Add support for WordPress 3.0's custom menus
-add_action( 'init', 'register_my_menu' );
- 
+add_action( 'init', 'register_my_menu' ); 
+
+// Adding theme support for custom backgrounds
+add_custom_background();
+
+// Adding feed links to header
+add_theme_support( 'automatic-feed-links' );
+
+// Adding theme support for post thumbnails
+add_theme_support( 'post-thumbnails' );
+
 //Register area for custom menu
 function register_my_menu() {
     register_nav_menu( 'primary-menu', __( 'Primary Menu' ) );
 }
-add_custom_background();
-add_theme_support( 'automatic-feed-links' );
 ?>
